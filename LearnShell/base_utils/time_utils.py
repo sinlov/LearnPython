27bd962860023ac60932d50860bee9ac3beddcc7
sinlov <sinlovgmppt@gmail.com>
2015-12-12 19:59:31 +0800

    Update utils of time

--- LearnShell/base_utils/time_utils.py
+++ LearnShell/base_utils/time_utils.py
@@ -4,7 +4,10 @@ __author__ = 'sinlov'
 import time
 import datetime
 
+
 def default():
     return time.strftime("%Y-%m-%d %H:%M:%S  %s")
+
+
 def microsecond():
     return int(datetime.datetime.now().microsecond)
