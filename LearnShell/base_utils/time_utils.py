fa806c64399077204c245414ab203cb054e1acee
sinlov <sinlovgmppt@gmail.com>
2015-12-14 02:04:02 +0800

    update utils

--- LearnShell/base_utils/time_utils.py
+++ LearnShell/base_utils/time_utils.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 __author__ = 'sinlov'
 
+import os
 import time
 import datetime
 
@@ -14,5 +15,25 @@ def microsecond():
 
 
 def str_format():
-    result = [str(time.strftime("%Y-%m-%d %H:%M:%S")), " .", str(microsecond())]
-    return "".join(result)
+    res = [str(time.strftime("%Y-%m-%d %H:%M:%S")), " .", str(microsecond())]
+    return ''.join(res)
+
+
+def file_format():
+    res = [str(time.strftime("%Y_%m_%d %H_%M_%S")), '.', str(microsecond())]
+    return ''.join(res)
+
+
+def log_path():
+    global middle_path
+    os_type = os.name
+    middle_path = '/build/out_log/'
+    if os_type == 'nt':
+        middle_path = '\\build\\out_log\\'
+    res = [os.getcwd(), middle_path]
+    return ''.join(res)
+
+
+def log_name():
+    res = [file_format(), '.log']
+    return ''.join(res)
