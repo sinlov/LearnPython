12b8250abfc6c3432380d868b878060dcc7d0b7b
sinlov <sinlovgmppt@gmail.com>
2016-03-17 19:31:50 +0800

    Add StringDemo full

--- LearnShell/RegularExpressions.py
+++ LearnShell/RegularExpressions.py
@@ -10,13 +10,17 @@ code_2 = '#dadad'
 code_3 = '00:12:12 --> 00:12:14'
 code_4 = '12351454'
 code_5 = '\n'
+code_6 = r'abc fsind me you are funny'
 
 reg_code_1 = r'--------------'
 reg_code_2 = r'/A/d*[/n]/Z'
 reg_code_3 = r'/d --> /d'
 reg_return = r'\n'
+reg_find = r'find'
 
-if re.match(reg_return, code_5):
+if re.match(reg_find, code_6):
     print 'yes'
 else:
     print 'no'
+
+print code_6.find(reg_find)
