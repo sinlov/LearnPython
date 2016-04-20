80fe0fac2217a81a7c4c2a83b617e05a6631e8bb
sinlov <sinlovgmppt@gmail.com>
2016-04-20 15:24:55 +0800

    add properties utils and test file

---
+++ LearnShell/base_utils/properties_utils.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+import ConfigParser
+
+__author__ = 'sinlov'
+
+
+class PrettyConfigParser(ConfigParser.ConfigParser):
+    def optionxform(self, option):
+        return option
+
+
+cf = PrettyConfigParser()
+cf.read('test.properties')
+
+section = cf.sections()
+print 'section: ', section
+
+opt = cf.options('dynamic')
+print 'option: ', opt
+
+mainAct = cf.get('dynamic', 'mainActivity')
+print 'mainAct: ', mainAct
+
+cf.set('dynamic', 'mainActivity', 'demo.MainActivity')
+cf.write(open('test.properties', 'w'))
