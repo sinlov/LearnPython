550d21f274a514dc722ad56bc4517d2b287172e1
sinlov <sinlovgmppt@gmail.com>
2016-04-21 00:25:23 +0800

    add logging_head

---
+++ LearnShell/base_utils/logging_head.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import logging
+
+__author__ = 'sinlov'
+
+
+def bless():
+    """
+        to keep Buddha align , we must keep it above this line
+    """
+    logging.info('=====================================================================================')
+    logging.info('////////////////////////////////////////////////////////////////////')
+    logging.info('//                            _ooOoo_                             //')
+    logging.info('//                           o8888888o                            //')
+    logging.info('//                           88" . "88                            //')
+    logging.info('//                           (| -_- |)                            //')
+    logging.info('//                           O\  =  /O                            //')
+    logging.info('//                        ____/`---\'\____                         //')
+    logging.info('//                      .\'  \\|     |//  `.                        //')
+    logging.info('//                     /  \\|||  :  |||//  \                       //')
+    logging.info('//                    /  _||||| -:- |||||-  \                     //')
+    logging.info('//                    |   | \\\  -  /// |    |                     //')
+    logging.info('//                    | \_|  \'\'\---/\'\'  |   |                     //')
+    logging.info('//                    \  .-\__  `-`  ___/-. /                     //')
+    logging.info('//                  ___`. .\'  /--.--\  `. . ___                   //')
+    logging.info('//                ."" \'<  `.___\_<|>_/___.\'  >\'"".                //')
+    logging.info('//              | | :  `- \`.;`\ _ /`;.`/ - ` : | |               //')
+    logging.info('//              \  \ `-.   \_ __\ /__ _/   .-` /  /               //')
+    logging.info('//        ========`-.____`-.___\_____/___.-`____.-\'========       //')
+    logging.info('//                             `=---=\'                            ')
+    logging.info('//        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^      //')
+    logging.info('                      Buddha Bless non-existent Bugs                  ')
+    logging.info('////////////////////////////////////////////////////////////////////')
