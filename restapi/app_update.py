412921a2a4a848f7e6ce157cfa02970f0d3dd2c9
sinlov <sinlovgmppt@gmail.com>
2018-03-22 19:35:53 +0800

    add app_update API

---
+++ restapi/app_update.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import json
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import redirect
+
+app = Flask(__name__)
+
+update_data = """
+{
+  "msg":"update info",
+  "pn":"",
+  "vc":"1000000",
+  "vn":"1.0.0-SNAPSHOT",
+  "download_url":"",
+  "channel":"dakehu",
+  "extra":"",
+  "force": 0
+}"""
+
+
+@app.route('/band/v1/app/update', methods=['GET', 'POST'])
+def band_app_update():
+    if request.method == 'GET':
+        a = request.get_data()
+        update_json_obj = json.loads(update_data)
+        # return json.dumps(update_json_obj["data"])
+        return jsonify(update_json_obj)
+    else:
+        return '<h1>only support method GET！</h1>'
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return '<h1>hello ！</h1>'
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=17777,
+        debug=True
+    )
